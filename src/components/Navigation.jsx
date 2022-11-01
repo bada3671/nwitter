0b142754aaa59af2d8fa4bc0b44393adfc952566
bada3670 <iykl3670@gmail.com>
2022-11-01 14:34:47 +0900

    before refactoring

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -1,7 +1,10 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
-export default function Navigation() {
+export default function Navigation({ userObj }) {
+  // 소셜 로그인이면 displayName이 있고, 그냥 로그인이면 없으므로
+  const profileName = userObj.displayName || userObj.email.split('@')[0];
+
   return (
     <nav>
       <ul>
@@ -9,7 +12,7 @@ export default function Navigation() {
           <Link to={'/'}>Home</Link>
         </li>
         <li>
-          <Link to={'/profile'}>My Profile</Link>
+          <Link to={'/profile'}>{profileName}의 Profile</Link>
         </li>
       </ul>
     </nav>
--- src/routes/Profile.jsx
+++ src/routes/Profile.jsx
@@ -1,16 +1,60 @@
-import React from 'react';
-import { authService } from '../fb';
+import React, { useEffect, useState } from 'react';
+import { authService, dbService } from '../fb';
 import { useHistory } from 'react-router-dom';
 
-function Profile() {
+function Profile({ userObj, refreshUser }) {
   const history = useHistory();
+  // 소셜 로그인이면 displayName이 있고, 그냥 로그인이면 없으므로
+  const profileName = userObj.displayName || userObj.email.split('@')[0];
+
+  const [newDisplayName, setNewDisplayName] = useState(profileName);
+
   const onLogOutClick = () => {
     authService.signOut();
     history.push('/');
   };
 
+  const getMyNweets = async () => {
+    const nweets = await dbService
+      .collection('nweets')
+      .where('creatorId', '==', userObj.uid)
+      .orderBy('createdAt')
+      .get();
+    console.log(nweets.docs.map((doc) => doc.data()));
+  };
+
+  useEffect(() => {
+    getMyNweets();
+  }, []);
+
+  const onChange = (event) => {
+    const {
+      target: { value },
+    } = event;
+    setNewDisplayName(value);
+  };
+
+  const onSubmit = async (event) => {
+    event.preventDefault();
+    if (profileName !== newDisplayName) {
+      await userObj.updateProfile({
+        displayName: newDisplayName,
+      });
+      refreshUser();
+    }
+  };
+
   return (
     <>
+      <form onSubmit={onSubmit}>
+        <input
+          onChange={onChange}
+          type={'text'}
+          placeholder={'Display name'}
+          value={newDisplayName}
+        />
+        <input type={'submit'} value={'Update Profile'} />
+      </form>
       <button onClick={onLogOutClick}>Log Out</button>
     </>
   );
